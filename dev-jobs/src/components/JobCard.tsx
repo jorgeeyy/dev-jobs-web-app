1f219be33ddc35bd2d2561068e9d2775c96e14f3
JorgeQ1 <jqubez@gmail.com>
2025-02-24 17:07:00 +0000

    homepage screen

---
+++ dev-jobs/src/components/JobCard.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+
+interface CardProps {
+  id: number;
+  company: string;
+  logo: string;
+  logoBackground: string;
+  position: string;
+  postedAt: string;
+  contract: string;
+  location: string;
+  //   website: string;
+  //   apply: string;
+  //   description: string;
+  //   requirements: [content: string, items: string];
+}
+
+const Card: React.FC<CardProps> = ({
+  //   id,
+  company,
+  logo,
+  logoBackground,
+  position,
+  postedAt,
+  contract,
+  location,
+}) => {
+  return (
+    // <div className="max-w-sm rounded-2xl overflow-hidden shadow-lg bg-white p-4 relative">
+    <div className="bg-white h-64 w-96 pt-8 mt-10 rounded-xl relative">
+      <div
+        style={{ backgroundColor: logoBackground }}
+        className="h-12 w-14 pl-2 pt-2 pb-1 rounded-lg ml-7 absolute z-10 -top-6"
+      >
+        {/* image logo  */}
+        <img src={logo} alt="logo" className="h-5 w-10 mt-2" />
+      </div>
+      <div className="mt-3 ml-7 mb-3 flex space-x-2">
+        <p className="text-[#6E8098]">{postedAt}</p>
+        <span className="text-[#6E8098]">.</span>
+        <p className="text-[#6E8098]">{contract}</p>
+      </div>
+      <div className="mt-5 ml-7">
+        <p className="font-bold h-8">{position}</p>
+      </div>
+      <div className="ml-7">
+        <p className="text-[#6E8098]">{company}</p>
+      </div>
+      <div className="mt-10 ml-7">
+        <p className="text-[#5964E0] font-medium">{location}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
--- dev-jobs/src/components/NavBar.tsx
+++ dev-jobs/src/components/NavBar.tsx
@@ -1,19 +1,17 @@
 const NavBar: React.FC = () => {
   return (
-    <div className="bg-[url(../src/assets/desktop/bg-pattern-header.svg)] bg-cover bg-no-repeat rounded-bl-[70px] h-30 ml-5 grid grid-cols-3 pt-10">
-      <div className="border-1 border-yellow-500 ml-30 h-10 w-50">
-        <p>dev jobs</p>
-      </div>
-      <br />
-      <div className="border-2 border-red-500 mr-35 h-10">
-        {/* <p className="flex justify-end mr-3">toggle div</p>
-                <p className="flex justify-end mr-3">toggle div</p>
-                <p className="flex justify-end mr-3">toggle div</p> */}
-        <label className="relative inline-flex cursor-pointer items-center">
-          <input id="switch" type="checkbox" className="peer sr-only" />
-          {/* <label for="switch" className="hidden"></label> */}
-          <div className="peer h-6 w-11 rounded-full border bg-slate-200 after:absolute after:left-[2px] after:top-0.5 after:h-5 after:w-5 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:bg-slate-800 peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:ring-green-300"></div>
-        </label>
+    <div className="h-50 flex flex-col relative">
+      <div className="bg-[url(../src/assets/desktop/bg-pattern-header.svg)] bg-cover bg-no-repeat rounded-bl-[70px] h-30 ml-5 grid grid-cols-3 pt-10">
+        <div className="border-1 border-yellow-500 ml-30 h-10 w-50">
+          <p>dev jobs</p>
+        </div>
+        <br />
+        <div className="border-2 border-red-500 mr-35 h-10">
+          <label className="relative inline-flex cursor-pointer items-center">
+            <input id="switch" type="checkbox" className="peer sr-only" />
+            <div className="peer h-6 w-11 rounded-full border bg-slate-200 after:absolute after:left-[2px] after:top-0.5 after:h-5 after:w-5 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:bg-slate-800 peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:ring-green-300"></div>
+          </label>
+        </div>
       </div>
     </div>
   );
