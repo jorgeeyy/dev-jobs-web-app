b891020c640013e186b3bc9831b19a3c7dcab4f0
JorgeQ1 <jqubez@gmail.com>
2025-02-24 07:35:58 +0000

    first commit

---
+++ dev-jobs/src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import NavBar from "./components/NavBar";
+import TopSection from "./components/TopSection";
+
+function App() {
+  return (
+    <>
+      <div className="bg-[#F4F6F8] relative">
+        <NavBar />
+        <TopSection />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ dev-jobs/src/components/TopSection.tsx
@@ -0,0 +1,46 @@
+const TopSection: React.FC = () => {
+  return (
+    <div className="bg-[#FFFFFF] border-1 border-black-400 h-20 ml-35 mr-35 grid grid-cols-3 absolute">
+      <div className="h-10 mt-5 flex space-x-2">
+        <img
+          src="../src/assets/desktop/icon-search.svg"
+          alt="search icon"
+          className="ml-3 mt-2"
+        />
+        <input
+          type="text"
+          placeholder="Filter by company,....."
+          className="mt-2 mb-2 mr-2 pl-2 h-8 w-90 pr-2"
+        />
+      </div>
+      <div className="h-10 mt-5 flex space-x-2">
+        <img
+          src="../src/assets/desktop/icon-location.svg"
+          alt="location icon"
+          className="ml-3 mt-1"
+        />
+        <input
+          type="text"
+          placeholder="Filter by location,....."
+          className="mt-1 mr-2 pl-2 h-8 w-90 pr-2"
+        />
+      </div>
+      <div className="h-10 mt-5 grid grid-cols-2">
+        <div className="flex space-x-4 ">
+          <input
+            type="checkbox"
+            className="w-4 h-4 ml-5 mt-3.5 checked:bg-blue-600 checked:border-blue-600"
+            id="job-type"
+          />
+          <label htmlFor="job-type" className="mt-2.5 mb-3.0">
+            Full Time
+          </label>
+        </div>
+        <button className="bg-[#5964E0] w-30 h-10 ml-5 px-4 py-2 text-[#FFFFFF] rounded-md">
+          Search
+        </button>
+      </div>
+    </div>
+  );
+};
+export default TopSection;
